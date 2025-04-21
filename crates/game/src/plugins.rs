72611afa7498766b051a727baea64f0d8fe14feb
William Quintal <william95quintalwilliam@outlook.com>
2025-04-21 15:12:51 -0400

    character animation working

--- crates/game/src/plugins.rs
+++ crates/game/src/plugins.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::*;
 
+use animation::D2AnimationPlugin;
 
 pub struct BaseZombieGamePlugin;
 
 impl Plugin for BaseZombieGamePlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(D2AnimationPlugin);
     }
 }
