1ca4ffda5607e4f74a147f08ac017677b48acf20
wquintal <williamquintal95@gmail.com>
2025-04-24 21:31:21 -0400

    Basic audio (#9)

---
+++ crates/game/src/audio/mod.rs
@@ -0,0 +1,21 @@
+
+
+use std::io::Cursor;
+
+use bevy::prelude::*;
+use bevy_kira_audio::prelude::*;
+
+
+pub struct ZAudioPlugin {}
+
+impl Plugin for ZAudioPlugin {
+   fn build(&self, app: &mut App) {
+       app.add_plugins(AudioPlugin);
+       app.add_systems(Startup, play_loop);
+   } 
+    
+}
+
+fn play_loop(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    audio.play(asset_server.load("sounds/loop.ogg")).looped();
+}
