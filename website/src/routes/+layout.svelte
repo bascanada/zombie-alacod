8dda6b7a0e96a46ce3273eaf772ac109abce179f
wquintal <williamquintal95@gmail.com>
2025-05-07 04:05:41 -0400

    this

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -9,10 +9,16 @@
 <div class="h-full w-full flex flex-row">
   <div class="h-full z-50 bg-surface-100-800-token border-t border-surface-300-600-token">
     <Navigation.Rail>
+        {#snippet header()}
+          <Navigation.Tile label="" href="/"><img class="logo" src="/icons/android-chrome-192x192.png" alt="logo" /></Navigation.Tile>
+        {/snippet}
         {#snippet tiles()}
-        <Navigation.Tile label="Home" href="/"><House /></Navigation.Tile>
-        <Navigation.Tile label="Game" href="/game"><Gamepad /></Navigation.Tile>
-        <Navigation.Tile label="Settings" href="/settings"><Settings /></Navigation.Tile>
+          <Navigation.Tile label="Home" href="/"><House /></Navigation.Tile>
+          <Navigation.Tile label="Game" href="/game"><Gamepad /></Navigation.Tile>
+        {/snippet}
+
+        {#snippet footer()}
+          <Navigation.Tile label="Settings" href="/settings"><Settings /></Navigation.Tile>
         {/snippet}
     </Navigation.Rail>
   </div>
@@ -24,4 +30,10 @@
   </div>
 
 </div>
+
+<style>
+  .logo {
+    border-radius: 10px;
+  }
+</style>
 
